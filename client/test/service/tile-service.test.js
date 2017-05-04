49cd20b1f5e8912436ae737ae8fdd0d87e383545
Shaneel Sharan <shaneel.sharan@gmail.com>
2017-05-04 00:22:10 -0700

    Test for tile normals and transformation matrix

--- client/test/service/tile-service.test.js
+++ client/test/service/tile-service.test.js
@@ -3,14 +3,16 @@
 const service = require('../../main/source/service/tile-service');
 const vec3 = require('gl-matrix').vec3;
 const assert = require('chai').assert;
+const delta = 0.05;
 
 describe('tile-service#getRenderData', function() {
     it('returns correct data for a single tile at the origin', function() {
-        let tile = { loc: { x: 0, y: 1, z: 0 } };
         let hexRadius = 5;
+        let tile = { loc: { x: 0, y: 1, z: 0 } };
         let data = service.getRenderData(tile, hexRadius);
 
-        assertCorrectVertices(tile, hexRadius, data);
+        assertTranslationMatrix(data.transform, 0, 1, 0);
+        assertCorrectMeshData(tile, hexRadius, data);
     });
 
     it('returns correct data for adjacent tiles not at the origin', function() {
@@ -18,84 +20,124 @@ describe('tile-service#getRenderData', function() {
 
         let tile1 = { loc: { x: 4, y: 1, z: 4 } };
         let data1 = service.getRenderData(tile1, hexRadius);
-        assertCorrectVertices(tile1, hexRadius, data1);
+
+        assertTranslationMatrix(data1.transform, 60, 1, 69.3);
+        assertCorrectMeshData(tile1, hexRadius, data1);
 
         let tile2 = { loc: { x: 5, y: 2, z: 4 } };
         let data2 = service.getRenderData(tile2, hexRadius);
-        assertCorrectVertices(tile2, hexRadius, data2);
+
+        assertTranslationMatrix(data2.transform, 75, 2, 77.9);
+        assertCorrectMeshData(tile2, hexRadius, data2);
     });
 });
 
-function assertCorrectVertices(tile, hexRadius, data) {
-    let expected = getExpectedVertices(tile, hexRadius);
+/* Assert that the transformation matrix is a translation matrix with
+ * with the given x, y, z values. */
+function assertTranslationMatrix(transformationMatrix, x, y, z) {
+    assert.equal(transformationMatrix[0], 1);
+    assert.equal(transformationMatrix[1], 0);
+    assert.equal(transformationMatrix[2], 0);
+    assert.equal(transformationMatrix[3], 0);
+
+    assert.equal(transformationMatrix[4], 0);
+    assert.equal(transformationMatrix[5], 1);
+    assert.equal(transformationMatrix[6], 0);
+    assert.equal(transformationMatrix[7], 0);
+
+    assert.equal(transformationMatrix[8], 0);
+    assert.equal(transformationMatrix[9], 0);
+    assert.equal(transformationMatrix[10], 1);
+    assert.equal(transformationMatrix[11], 0);
+
+    assert.approximately(transformationMatrix[12], x, delta);
+    assert.approximately(transformationMatrix[13], y, delta);
+    assert.approximately(transformationMatrix[14], z, delta);
+    assert.equal(transformationMatrix[15], 1);
+}
+
+/* Assert that the tile with the given hexRadius has the correct mesh data. */
+function assertCorrectMeshData(tile, hexRadius, data) {
+    let expectedVertices = getExpectedVertices(tile, hexRadius);
+    let expectedNormals = getExpectedNormals();
     let index = 0;
 
-    assertCorrectVertex(data, index++, expected.center);
-    assertCorrectVertex(data, index++, expected.right);
-    assertCorrectVertex(data, index++, expected.upperRight);
+    assertCorrectVertex(data, index++, expectedVertices.center, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.right, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.upperRight, expectedNormals.up);
 
-    assertCorrectVertex(data, index++, expected.center);
-    assertCorrectVertex(data, index++, expected.upperRight);
-    assertCorrectVertex(data, index++, expected.upperLeft);
+    assertCorrectVertex(data, index++, expectedVertices.center, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.upperRight, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.upperLeft, expectedNormals.up);
 
-    assertCorrectVertex(data, index++, expected.center);
-    assertCorrectVertex(data, index++, expected.upperLeft);
-    assertCorrectVertex(data, index++, expected.left);
+    assertCorrectVertex(data, index++, expectedVertices.center, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.upperLeft, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.left, expectedNormals.up);
 
-    assertCorrectVertex(data, index++, expected.center);
-    assertCorrectVertex(data, index++, expected.left);
-    assertCorrectVertex(data, index++, expected.lowerLeft);
+    assertCorrectVertex(data, index++, expectedVertices.center, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.left, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.lowerLeft, expectedNormals.up);
 
-    assertCorrectVertex(data, index++, expected.center);
-    assertCorrectVertex(data, index++, expected.lowerLeft);
-    assertCorrectVertex(data, index++, expected.lowerRight);
+    assertCorrectVertex(data, index++, expectedVertices.center, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.lowerLeft, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.lowerRight, expectedNormals.up);
 
-    assertCorrectVertex(data, index++, expected.center);
-    assertCorrectVertex(data, index++, expected.lowerRight);
-    assertCorrectVertex(data, index++, expected.right);
+    assertCorrectVertex(data, index++, expectedVertices.center, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.lowerRight, expectedNormals.up);
+    assertCorrectVertex(data, index++, expectedVertices.right, expectedNormals.up);
 
-    assertCorrectBase(data, index, expected.right, expected.rightBase, expected.lowerRight, expected.lowerRightBase);
+    assertCorrectBase(data, index, expectedVertices.right, expectedVertices.rightBase,
+        expectedVertices.lowerRight, expectedVertices.lowerRightBase, expectedNormals.southeast);
     index+=6;
 
-    assertCorrectBase(data, index, expected.lowerRight, expected.lowerRightBase, expected.lowerLeft, expected.lowerLeftBase);
+    assertCorrectBase(data, index, expectedVertices.lowerRight, expectedVertices.lowerRightBase,
+        expectedVertices.lowerLeft, expectedVertices.lowerLeftBase, expectedNormals.south);
     index+=6;
 
-    assertCorrectBase(data, index, expected.lowerLeft, expected.lowerLeftBase, expected.left, expected.leftBase);
+    assertCorrectBase(data, index, expectedVertices.lowerLeft, expectedVertices.lowerLeftBase,
+        expectedVertices.left, expectedVertices.leftBase, expectedNormals.southwest);
     index+=6;
 
-    assertCorrectBase(data, index, expected.left, expected.leftBase, expected.upperLeft, expected.upperLeftBase);
+    assertCorrectBase(data, index, expectedVertices.left, expectedVertices.leftBase,
+        expectedVertices.upperLeft, expectedVertices.upperLeftBase, expectedNormals.northwest);
     index+=6;
 
-    assertCorrectBase(data, index, expected.upperLeft, expected.upperLeftBase, expected.upperRight, expected.upperRightBase);
+    assertCorrectBase(data, index, expectedVertices.upperLeft, expectedVertices.upperLeftBase,
+        expectedVertices.upperRight, expectedVertices.upperRightBase, expectedNormals.north);
     index+=6;
 
-    assertCorrectBase(data, index, expected.upperRight, expected.upperRightBase, expected.right, expected.rightBase);
+    assertCorrectBase(data, index, expectedVertices.upperRight, expectedVertices.upperRightBase,
+        expectedVertices.right, expectedVertices.rightBase, expectedNormals.northeast);
 }
-
-function assertCorrectBase(data, index, corner1, base1, corner2, base2) {
-    assertCorrectVertex(data, index++, corner1);
-    assertCorrectVertex(data, index++, corner2);
-    assertCorrectVertex(data, index++, base1);
-    assertCorrectVertex(data, index++, corner2);
-    assertCorrectVertex(data, index++, base2);
-    assertCorrectVertex(data, index++, base1);
+function assertCorrectBase(data, index, corner1, base1, corner2, base2, normal) {
+    assertCorrectVertex(data, index++, corner1, normal);
+    assertCorrectVertex(data, index++, corner2, normal);
+    assertCorrectVertex(data, index++, base1, normal);
+    assertCorrectVertex(data, index++, corner2, normal);
+    assertCorrectVertex(data, index++, base2, normal);
+    assertCorrectVertex(data, index++, base1, normal);
 }
-
-function assertCorrectVertex(data, index, vertex) {
-    const delta = 0.05;
-
+function assertCorrectVertex(data, index, vertex, normal) {
     // Apply the transformation to the vertex before doing the comparison
-    let actual = vec3.fromValues(
+    let actualVertex = vec3.fromValues(
         data.mesh.vertices[3 * data.mesh.indices[index]],
         data.mesh.vertices[3 * data.mesh.indices[index] + 1],
         data.mesh.vertices[3 * data.mesh.indices[index] + 2]);
-    actual = vec3.transformMat4(vec3.create(), actual, data.transform);
+    actualVertex = vec3.transformMat4(vec3.create(), actualVertex, data.transform);
 
-    assert.approximately(actual[0], vertex[0], delta);
-    assert.approximately(actual[1], vertex[1], delta);
-    assert.approximately(actual[2], vertex[2], delta);
+    assert.approximately(actualVertex[0], vertex[0], delta);
+    assert.approximately(actualVertex[1], vertex[1], delta);
+    assert.approximately(actualVertex[2], vertex[2], delta);
+
+    let actualNormal = vec3.fromValues(
+        data.mesh.normals[3 * data.mesh.indices[index]],
+        data.mesh.normals[3 * data.mesh.indices[index] + 1],
+        data.mesh.normals[3 * data.mesh.indices[index] + 2]);
+
+    assert.approximately(actualNormal[0], normal[0], delta);
+    assert.approximately(actualNormal[1], normal[1], delta);
+    assert.approximately(actualNormal[2], normal[2], delta);
 }
-
 function getExpectedVertices(tile, hexRadius) {
     const centerX = tile.loc.x * hexRadius * 1.5;
     const centerZ = tile.loc.z * hexRadius * 0.866 * 2;
@@ -119,5 +161,17 @@ function getExpectedVertices(tile, hexRadius) {
         leftBase:       [centerX - hexRadius,       0, centerZ + shiftFactor],
         upperLeftBase:  [centerX - (hexRadius / 2), 0, centerZ - (hexRadius * 0.866) + shiftFactor],
         upperRightBase: [centerX + (hexRadius / 2), 0, centerZ - (hexRadius * 0.866) + shiftFactor],
+    }
+}
+function getExpectedNormals() {
+    // If we consider -z direction "north" and +x direction "east"
+    return {
+        southeast: [0.866, 0, 0.5],
+        south:     [0, 0, 1],
+        southwest: [-0.866, 0, 0.5],
+        northwest: [-0.866, 0, -0.5],
+        north:     [0, 0, -1],
+        northeast: [0.866, 0, -0.5],
+        up:        [0, 1, 0]
     }
 }

2a9a068f550710dcd97c9259b076d8b4c8168b43
Shaneel Sharan <shaneel.sharan@gmail.com>
2017-05-28 10:39:47 -0700

    Simplify array push in tile service

--- client/main/source/service/tile-service.js
+++ client/main/source/service/tile-service.js
@@ -29,33 +29,33 @@ export default {
 
         // Add center vertex
         let center = vec3.fromValues(0, 0, 0);
-        vertices.push(center[0]); vertices.push(center[1]); vertices.push(center[2]);
-        normals.push(0); normals.push(1); normals.push(0); // Normal faces upward
+        vertices.push(center[0], center[1], center[2]);
+        normals.push(0, 1, 0); // Normal faces upward
 
         // Add the 6 corner vertices
         let corner = vec3.fromValues(hexRadius, 0, 0);
         for (let j = 0; j < 6; j++) {
-            vertices.push(corner[0]); vertices.push(corner[1]); vertices.push(corner[2]);
-            normals.push(0); normals.push(1); normals.push(0); // Normal faces upward
+            vertices.push(corner[0], corner[1], corner[2]);
+            normals.push(0, 1, 0); // Normal faces upward
             corner = vec3.rotateY(vec3.create(), corner, center, glMatrix.toRadian(60.0));
         }
 
         // Add texcoords
-        texcoords.push(0.5); texcoords.push(0.5); // Middle of image for center of tile
-        texcoords.push(1.0); texcoords.push(0.5);
-        texcoords.push(0.75); texcoords.push(1.0);
-        texcoords.push(0.25); texcoords.push(1.0);
-        texcoords.push(0); texcoords.push(0.5);
-        texcoords.push(0.25); texcoords.push(0);
-        texcoords.push(0.75); texcoords.push(0);
+        texcoords.push(0.5, 0.5); // Middle of image for center of tile
+        texcoords.push(1.0, 0.5);
+        texcoords.push(0.75, 1.0);
+        texcoords.push(0.25, 1.0);
+        texcoords.push(0, 0.5);
+        texcoords.push(0.25, 0);
+        texcoords.push(0.75, 0);
 
         // Add indices
-        indices.push(0); indices.push(1); indices.push(2);
-        indices.push(0); indices.push(2); indices.push(3);
-        indices.push(0); indices.push(3); indices.push(4);
-        indices.push(0); indices.push(4); indices.push(5);
-        indices.push(0); indices.push(5); indices.push(6);
-        indices.push(0); indices.push(6); indices.push(1);
+        indices.push(0, 1, 2);
+        indices.push(0, 2, 3);
+        indices.push(0, 3, 4);
+        indices.push(0, 4, 5);
+        indices.push(0, 5, 6);
+        indices.push(0, 6, 1);
 
         /* Now we create the base of the tile to make the tile 3-dimensional.
            Each side of the base is made up of 4 vertices: two adjacent 'corner'
@@ -68,54 +68,54 @@ export default {
         let baseNormal = vec3.fromValues(0.866, 0, 0.5);  // Starting with the base that faces +x/+z direction
         for (let j = 0; j < 6; j++) {
             // Add the corner vertex
-            vertices.push(baseCorner[0]); vertices.push(baseCorner[1]); vertices.push(baseCorner[2]);
+            vertices.push(baseCorner[0], baseCorner[1], baseCorner[2]);
             // Add the vertex below the corner vertex
-            vertices.push(baseCorner[0]); vertices.push(-tile.loc.y); vertices.push(baseCorner[2]);
+            vertices.push(baseCorner[0], -tile.loc.y, baseCorner[2]);
 
             baseCorner = vec3.rotateY(vec3.create(), baseCorner, center, glMatrix.toRadian(-60.0));
 
             // Add the new corner vertex
-            vertices.push(baseCorner[0]); vertices.push(baseCorner[1]); vertices.push(baseCorner[2]);
+            vertices.push(baseCorner[0], baseCorner[1], baseCorner[2]);
             // Add the vertex below the new corner vertex
-            vertices.push(baseCorner[0]); vertices.push(-tile.loc.y); vertices.push(baseCorner[2]);
+            vertices.push(baseCorner[0], -tile.loc.y, baseCorner[2]);
 
             // Add the normals for this side of the base - they all face the same direction
             for (let k = 0; k < 4; k++) {
-                normals.push(baseNormal[0]); normals.push(baseNormal[1]); normals.push(baseNormal[2]);
+                normals.push(baseNormal[0], baseNormal[1], baseNormal[2]);
             }
             baseNormal = vec3.rotateY(vec3.create(), baseNormal, vec3.fromValues(0, 1, 0), glMatrix.toRadian(-60.0));
             baseNormal = vec3.normalize(vec3.create(), baseNormal);
 
             // Add texcoords for the base, same for each iteration of the loop
-            texcoords.push(0.75); texcoords.push(1.0);
-            texcoords.push(0.75); texcoords.push(0.0);
-            texcoords.push(0.25); texcoords.push(1.0);
-            texcoords.push(0.25); texcoords.push(0.0);
+            texcoords.push(0.75, 1.0);
+            texcoords.push(0.75, 0.0);
+            texcoords.push(0.25, 1.0);
+            texcoords.push(0.25, 0.0);
         }
 
         // Add indices for the first side of base
-        indices.push(7); indices.push(9); indices.push(8);
-        indices.push(9); indices.push(10); indices.push(8);
+        indices.push(7, 9, 8);
+        indices.push(9, 10, 8);
 
         // Add indices for the second side of base
-        indices.push(11); indices.push(13); indices.push(12);
-        indices.push(13); indices.push(14); indices.push(12);
+        indices.push(11, 13, 12);
+        indices.push(13, 14, 12);
 
         // Add indices for the third side of base
-        indices.push(15); indices.push(17); indices.push(16);
-        indices.push(17); indices.push(18); indices.push(16);
+        indices.push(15, 17, 16);
+        indices.push(17, 18, 16);
 
         // Add indices for the fourth side of base
-        indices.push(19); indices.push(21); indices.push(20);
-        indices.push(21); indices.push(22); indices.push(20);
+        indices.push(19, 21, 20);
+        indices.push(21, 22, 20);
 
         // Add indices for the fifth side of base
-        indices.push(23); indices.push(25); indices.push(24);
-        indices.push(25); indices.push(26); indices.push(24);
+        indices.push(23, 25, 24);
+        indices.push(25, 26, 24);
 
         // Add indices for the sixth side of base
-        indices.push(27); indices.push(29); indices.push(28);
-        indices.push(29); indices.push(30); indices.push(28);
+        indices.push(27, 29, 28);
+        indices.push(29, 30, 28);
 
         /* Calculate the transformation for this tile. The transformation is defined
            by the loc values in 'tile' as well as the 'hexRadius'.

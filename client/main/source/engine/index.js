f235089c9e4e1d7aa1e9bab129632b3d5dc85478
Shaneel Sharan <shaneel.sharan@gmail.com>
2017-07-06 22:20:38 -0700

    Export root scene node to be used outside engine

--- client/main/source/engine/index.js
+++ client/main/source/engine/index.js
@@ -1,10 +1,12 @@
+import SceneNode from './render/node/scene-node';
+
 export {default as KeyboardHandler} from './input/keyboard-handler'
 export {default as MouseHandler} from './input/mouse-handler'
 
 export {default as Mesh} from './render/mesh/mesh'
 export {default as FlatQuad} from './render/mesh/flat-quad'
 
-export {default as SceneNode} from './render/node/scene-node'
+export const RootSceneNode = new SceneNode();
 export {default as GeometryNode} from './render/node/geometry-node'
 export {default as LightNode} from './render/node/light-node'
 
--- client/main/source/main-loop.js
+++ client/main/source/main-loop.js
@@ -2,7 +2,7 @@ import {
     KeyboardHandler,
     MouseHandler,
     FlatQuad,
-    SceneNode,
+    RootSceneNode,
     GeometryNode,
     LightNode,
     Material,
@@ -27,7 +27,6 @@ export function begin(MainComponent) {
 
     // Build a scene graph from test tile data
     const hexRadius = 10;
-    const rootNode = new SceneNode();
     const lightNode1 = new LightNode(mat4.fromTranslation(mat4.create(), vec3.fromValues(60, 20, 60)),
             new Light({id: 'light1', type: "POINT",
                 direction: {x: 0, y: -1, z: 0},
@@ -54,14 +53,14 @@ export function begin(MainComponent) {
         );
     lightNode1.addChild(new GeometryNode(mat4.create(), {mesh: new FlatQuad(), material: lightIconMaterial}));
     lightNode2.addChild(new GeometryNode(mat4.create(), {mesh: new FlatQuad(), material: lightIconMaterial}));
-    rootNode.addChild(lightNode1);
-    rootNode.addChild(lightNode2);
+    RootSceneNode.addChild(lightNode1);
+    RootSceneNode.addChild(lightNode2);
     for (let i = 0; i < 50; i++) {
         for (let j = 0; j < 50; j++) {
             const tile = { loc: { x: i, y: i+j+1, z: j } };
             const renderData = TileService.getRenderData(tile, hexRadius);
             const child = new GeometryNode(renderData.localMatrix, {mesh: renderData.mesh, material: defaultMaterial});
-            rootNode.addChild(child);
+            RootSceneNode.addChild(child);
 
             if (i < 2 && j < 2) {
                 child.addChild(new GeometryNode(mat4.fromTranslation(mat4.create(), vec3.fromValues(0, 10, 0)),
@@ -93,7 +92,7 @@ export function begin(MainComponent) {
     // Start main loop
     function mainLoop() {
         KeyboardHandler.processKeys();
-        Renderer.render(rootNode);
+        Renderer.render(RootSceneNode);
         updateFPS();
         requestAnimationFrame(mainLoop);
     }

dc754dd457271a99158885249a52e38cf6bddf0d
Shaneel Sharan <shaneel.sharan@gmail.com>
2017-08-06 17:26:40 -0700

    Switch to async/await in file router

--- server/main/router/file-router.js
+++ server/main/router/file-router.js
@@ -13,55 +13,38 @@ const upload = multer({});
 export const FileRouterEndpoint = "/files";
 export const FileRouter = router;
 
-router.get('/view', (req, res) => {
-    const promise = getAllFileMetadata();
-    promise.then((metadata) => {
-        res.render('file', {fileEndpoint: FileRouterEndpoint, files: metadata});
-    })
-    .catch(err => {
-        res.status(500).send(err);
-    })
+router.get('/view', async (req, res) => {
+    const metadata = await getAllFileMetadata();
+    res.render('file', {fileEndpoint: FileRouterEndpoint, files: metadata});
 });
 
-router.get('/:id/content', (req, res) => {
-    const promise = getFileMetadata(req.params.id);
-    promise.then((metadata) => {
-        if (metadata === null) {
-            res.status(400).json(`{"error": "No file with id ${req.params.id}"}`);
-        } else {
-            res.attachment(metadata.filename);
-            getFileContentStream(req.params.id).pipe(res);
-        }
-    })
-    .catch(err => {
-        res.status(500).send(err);
-    })
+router.get('/:id/content', async (req, res) => {
+    const metadata = await getFileMetadata(req.params.id);
+    if (metadata === null) {
+        res.status(400).json(`{"error": "No file with id ${req.params.id}"}`);
+    } else {
+        res.attachment(metadata.filename);
+        getFileContentStream(req.params.id).pipe(res);
+    }
 });
 
-router.get('/:id/metadata', (req, res) => {
-    const promise = getFileMetadata(req.params.id);
-    promise.then((metadata) => {
-        if (metadata === null) {
-            res.status(400).json(`{"error": "No file with id ${req.params.id}"}`);
-        } else {
-            res.status(200).send(metadata);
-        }
-    })
-    .catch(err => {
-        res.status(500).send(err);
-    })
+router.get('/:id/metadata', async (req, res) => {
+    const metadata = await getFileMetadata(req.params.id);
+    if (metadata === null) {
+        res.status(400).json(`{"error": "No file with id ${req.params.id}"}`);
+    } else {
+        res.status(200).send(metadata);
+    }
 });
 
-router.post('/', upload.any(), (req, res) => {
+router.post('/', upload.any(), async (req, res) => {
     if (!req.files) {
         res.status(400).send('No files to upload');
+        return;
     }
-    const promises = uploadFiles(req.files);
-    Promise.all(promises)
-        .then((files) => {
-            res.status(201).send(files);
-        })
-        .catch(err => {
-            res.status(500).send(err);
-        })
+    let files = [];
+    for (let file of uploadFiles(req.files)) {
+        files.push(await file);
+    }
+    res.status(201).send(files);
 });

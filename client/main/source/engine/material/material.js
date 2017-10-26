e9a563f84d0cb84a76ada63c3a9b3e37aeef8aa9
Shaneel Sharan <shaneel.sharan@gmail.com>
2017-10-25 23:36:25 -0700

    Change default texture color from blue to grey

--- client/main/source/engine/material/material.js
+++ client/main/source/engine/material/material.js
@@ -49,9 +49,9 @@ export function loadTextureAsync(imageSrc) {
     let texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    // Start with a default texture color (blue) until the image is loaded
+    // Start with a default texture color until the image is loaded
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-        new Uint8Array([0, 0, 255, 255]));
+        new Uint8Array([127, 127, 127, 255]));
 
     let image = new Image();
     image.src = imageSrc;

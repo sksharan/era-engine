3c876d6017d2093b7992f7e7fbb05e4d4b74474f
Shaneel Sharan <shaneel.sharan@gmail.com>
2020-11-22 14:49:35 -0800

    Update webpack and associated libraries, start client with npm start

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const extractSass = new ExtractTextPlugin({
-    filename: 'styles.css',
-    disable: process.env.NODE_ENV !== 'production'
-});
 
 module.exports = {
     entry: './main/index.js',
@@ -20,7 +14,6 @@ module.exports = {
             filename: 'dist/index.html',
             template: 'main/index.html'
         }),
-        extractSass
     ],
     resolve: {
         extensions: ['.js', '.jsx']
@@ -37,39 +30,39 @@ module.exports = {
                 loader: 'babel-loader',
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
                 }
             },
+            // https://v5.getbootstrap.com/docs/5.0/getting-started/webpack/#importing-precompiled-sass
             {
-                // https://github.com/webpack-contrib/sass-loader
-                // https://getbootstrap.com/docs/4.0/getting-started/webpack/
-                loader: extractSass.extract({
-                    use: [
-                        {
-                            loader: 'css-loader', // translates CSS into CommonJS modules
-                            options: {
-                                localIdentName: '[name]-[local]___[hash:base64:5]'
+                test: /\.(scss)$/,
+                use: [{
+                    // inject CSS to page
+                    loader: 'style-loader'
+                }, {
+                    // translates CSS into CommonJS modules
+                    loader: 'css-loader'
+                }, {
+                    // Run postcss actions
+                    loader: 'postcss-loader',
+                    options: {
+                        // `postcssOptions` is needed for postcss 8.x;
+                        // if you use postcss 7.x skip the key
+                        postcssOptions: {
+                            // postcss plugins, can be exported to postcss.config.js
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
                             }
-                        },
-                        {
-                            loader: 'postcss-loader', // Run post css actions
-                            options: {
-                                plugins: function() {
-                                    // post css plugins, can be exported to postcss.config.js
-                                    return [require('precss'), require('autoprefixer')];
-                                }
-                            }
-                        },
-                        {
-                            loader: 'sass-loader' // compiles SASS to CSS
                         }
-                    ],
-                    // Use style-loader in development
-                    fallback: 'style-loader' // inject CSS to page
-                }),
-                test: /\.(scss)$/
-            }
+                    }
+                }, {
+                    // compiles Sass to CSS
+                    loader: 'sass-loader'
+                }]
+              }
         ]
     },
     externals: {

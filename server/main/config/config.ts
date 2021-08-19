199576b8565d030493ceffbccedb7dd29265a0a2
Steven Collins <jsbobatea@outlook.com>
2021-08-18 22:32:40 -0700

    Chnage config to use env vars

--- server/main/config/config.ts
+++ server/main/config/config.ts
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 export const isTest = process.env.NODE_ENV === 'test';
 
 export const appConfig = {
     port: isTest ? 3001 : 3000,
     database: {
-        url: isTest ? 'mongodb://root:password@localhost/' : 'mongodb://root:password@localhost/',
-        dbName: isTest ? 'test' : 'app'
+        url: isTest ? process.env.CONNSTRING_TEST : process.env.CONNSTRING_PROD,
+        dbName: isTest ? process.env.DB_TEST : process.env.DB_PROD
     },
     redis: {
-        port: 6379,
-        host: 'localhost',
-        secret: 'hudfhu9dfh89fdh89df98h'
+        port: process.env.REDIS_PORT,
+        host: process.env.REDIS_HOST,
+        secret: process.env.REDIS_SECRET
     }
 };
